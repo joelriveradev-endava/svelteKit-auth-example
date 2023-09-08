a06b1ec44afa64e1feef77c5d1e02ff3008dd4d4
Joel Rivera <joel.rivera@endava.com>
2023-09-08 12:19:32 -0500

    feat: add support for both dev and prod envs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,21 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private'
+
+import {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_ID_DEV,
+  GITHUB_CLIENT_SECRET,
+  GITHUB_CLIENT_SECRET_DEV,
+  NODE_ENV
+} from '$env/static/private'
+
 import GitHub from "@auth/core/providers/github"
+
+const dev = NODE_ENV === 'development'
+
+const githubCredentials = {
+  clientId: dev ? GITHUB_CLIENT_ID_DEV : GITHUB_CLIENT_ID,
+  clientSecret: dev ? GITHUB_CLIENT_SECRET_DEV : GITHUB_CLIENT_SECRET
+}
 
 export const handle = SvelteKitAuth({
   pages: {
@@ -8,10 +23,7 @@ export const handle = SvelteKitAuth({
     'signOut': '/auth/signOut'
   },
   providers: [
-    GitHub({
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET
-    })
+    GitHub(githubCredentials)
   ],
   callbacks: {
     session: ({ session }) => {
