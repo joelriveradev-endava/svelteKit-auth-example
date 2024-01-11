8c838dc78c5e662f01b8c9967508e1cfccbbe7ca
Joel Rivera <Joel.Rivera@endava.com>
2024-01-11 15:13:38 -0600

    feat: add motion animations to posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,32 @@
-<script>
+<script lang='ts'>
+  import { stagger, animate, spring } from 'motion'
   export let data
 
   const authenticated = data.session
   const href = authenticated ? '/auth/signOut' : '/auth/signIn'
+  
+  let postsRef: HTMLDivElement
+
+  $: {
+    if(postsRef){
+      animate(
+        '.post', {
+          y: [-50, 0],
+          opacity: [0, 1],
+          scale: [0.8, 1.2, 1],
+        }, { delay: stagger(0.11), easing: spring() }
+      )
+    }
+  }
 </script>
 
 <main class='w-full'>
   <div class='md:max-w-xl'>
-    <h1 class='font-extrabold text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+    <h1 class='text-2xl font-extrabold md:text-3xl lg:text-4xl xl:text-5xl'>
       Welcome to the SvelteKit Auth Example!
     </h1>
   
-    <p class='text-slate-600 mt-6 font-light text-sm lg:text-lg'>
+    <p class='mt-6 text-sm font-light text-slate-600 lg:text-lg'>
       This is a simple, lightweight example of how 
       to implement a simple oauth solution for 
       SvelteKit apps.
@@ -32,10 +47,10 @@
     {#if data.posts}
       {@const { posts } = data}
 
-      <div class='w-full mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5'>
+      <div class='w-full mt-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5' bind:this={postsRef}>
         {#each posts as post}
-          <a href='/post/{post.id}'>
-            <div class='max-w-md border border-neutral-100 rounded-xl p-5 pb-6 hover:border-black hover:shadow-lg hover:shadow-gray-200 transition-all'>
+          <a href='/post/{post.id}' class='post'>
+            <div class='max-w-md rounded-xl border border-neutral-100 p-5 pb-6 transition-all hover:border-black hover:shadow-lg hover:shadow-gray-200'>
               <span class='block font-semibold mb-3 capitalize'>
                 {post.title}
               </span>
